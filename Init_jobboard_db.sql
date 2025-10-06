582fd089907e2dd6eade435608ee4985305b8940
Velcatt <antoinelacroix68@gmail.com>
2025-10-06 13:50:45 +0200

    sql file

--- Init_jobboard_db.sql
+++ Init_jobboard_db.sql
@@ -1,34 +1,45 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-CREATE TABLE `advertisements` (
-  `id` int(11) NOT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `brand` varchar(255) DEFAULT NULL,
-  `price` int(10) unsigned NOT NULL,
-  `stock` int(11) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+CREATE TABLE `company_categories` (
+    `category` varchar(50) PRIMARY KEY,
+)
+
+CREATE TABLE `user_categories` (
+    `category` varchar(50) PRIMARY KEY,
+)
 
 CREATE TABLE `companies` (
-  `id` int(11) NOT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `brand` varchar(255) DEFAULT NULL,
-  `price` int(10) unsigned NOT NULL,
-  `stock` int(11) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
+  `name` varchar(255) DEFAULT NULL,
+  FOREIGN KEY (company_category) REFERENCES company_categories(category),
+);
+
+CREATE TABLE `advertisements` (
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
+  `type` varchar(255) NOT NULL,
+  FOREIGN KEY (company_id) REFERENCES companies(id),
+  `date_posted` date unsigned NOT NULL,
+  `ad_text` text,
+);
 
 CREATE TABLE `people` (
-  `id` int(11) NOT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `brand` varchar(255) DEFAULT NULL,
-  `price` int(10) unsigned NOT NULL,
-  `stock` int(11) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+  `username` varchar(16) PRIMARY KEY,
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `email` varchar(255),
+  `phone` varchar(20),
+  FOREIGN KEY (category) REFERENCES user_categories(category),
+  `hashedpw` varchar(255) NOT NULL,
+);
 
 CREATE TABLE `applications` (
-  `id` int(11) NOT NULL,
-  `type` varchar(255) DEFAULT NULL,
-  `brand` varchar(255) DEFAULT NULL,
-  `price` int(10) unsigned NOT NULL,
-  `stock` int(11) unsigned NOT NULL DEFAULT '0'
-) ENGINE=InnoDB AUTO_INCREMENT=41 DEFAULT CHARSET=utf8;
+  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
+  FOREIGN KEY (ad_id) REFERENCES advertisements(id),
+  FOREIGN KEY (username) REFERENCES people(username) DEFAULT NULL,
+  `first_name` varchar(255),
+  `last_name` varchar(255),
+  `email` varchar(255),
+  `phone` varchar(20),
+  `message` text,
+)
