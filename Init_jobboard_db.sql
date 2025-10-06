1c24138d0de7ce05dc8907fc702356e2a1f3a88e
Velcatt <antoinelacroix68@gmail.com>
2025-10-06 16:40:00 +0200

    init db ok

--- Init_jobboard_db.sql
+++ Init_jobboard_db.sql
@@ -1,45 +1,43 @@
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-CREATE TABLE `company_categories` (
-    `category` varchar(50) PRIMARY KEY,
-)
-
-CREATE TABLE `user_categories` (
-    `category` varchar(50) PRIMARY KEY,
-)
-
-CREATE TABLE `companies` (
-  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `name` varchar(255) DEFAULT NULL,
-  FOREIGN KEY (company_category) REFERENCES company_categories(category),
+CREATE TABLE company_categories (
+    category varchar(50) PRIMARY KEY
 );
 
-CREATE TABLE `advertisements` (
-  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  `type` varchar(255) NOT NULL,
-  FOREIGN KEY (company_id) REFERENCES companies(id),
-  `date_posted` date unsigned NOT NULL,
-  `ad_text` text,
+CREATE TABLE user_categories (
+    category varchar(50) PRIMARY KEY
 );
 
-CREATE TABLE `people` (
-  `username` varchar(16) PRIMARY KEY,
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email` varchar(255),
-  `phone` varchar(20),
-  FOREIGN KEY (category) REFERENCES user_categories(category),
-  `hashedpw` varchar(255) NOT NULL,
+CREATE TABLE companies (
+  id SERIAL PRIMARY KEY,
+  name varchar(255) DEFAULT NULL,
+  company_category varchar(255) REFERENCES company_categories(category)
 );
 
-CREATE TABLE `applications` (
-  `id` int(11) AUTO_INCREMENT PRIMARY KEY,
-  FOREIGN KEY (ad_id) REFERENCES advertisements(id),
-  FOREIGN KEY (username) REFERENCES people(username) DEFAULT NULL,
-  `first_name` varchar(255),
-  `last_name` varchar(255),
-  `email` varchar(255),
-  `phone` varchar(20),
-  `message` text,
-)
+CREATE TABLE advertisements (
+  id SERIAL PRIMARY KEY,
+  type varchar(255) NOT NULL,
+  company_id INT REFERENCES companies(id),
+  date_posted date NOT NULL,
+  ad_text text
+);
+
+CREATE TABLE people (
+  username varchar(30) PRIMARY KEY,
+  first_name varchar(255),
+  last_name varchar(255),
+  dateofbirth date,
+  email varchar(255),
+  phone varchar(20),
+  category varchar(255) REFERENCES user_categories(category),
+  hashedpw varchar(255) NOT NULL
+);
+
+CREATE TABLE applications (
+  id SERIAL PRIMARY KEY,
+  ad_id int REFERENCES advertisements(id),
+  username varchar(30) REFERENCES people(username),
+  first_name varchar(255),
+  last_name varchar(255),
+  email varchar(255),
+  phone varchar(20),
+  application_message text
+);
